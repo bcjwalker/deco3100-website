b3e2cb154e171a90a1d7e0c81596a0120220326b
bcjwalker <125110890+bcjwalker@users.noreply.github.com>
2023-05-15 14:08:16 +1000

    missed html/scss/js folder

---
+++ public/script.js
@@ -0,0 +1,32 @@
+/*
+#############
+## Modules ##
+#############
+*/
+
+
+
+/*
+###############
+## Variables ##
+###############
+*/
+
+// DOM
+  // Get body
+  const pageBody = document.body;
+
+
+/*
+########################
+## Page Functionality ##
+########################
+*/
+
+// Called when page has been completely loaded
+window.addEventListener('load', function() {
+    console.log('Page loaded, starting postload scripts~')
+
+    // Init website functionality basics
+
+});
